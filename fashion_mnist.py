3b8831a438520480091c9ceecccaaf29d323eb11
liangjiaze <renmuda@163.com>
2021-01-08 16:28:19 +0800

    修改显示参数，以黑白灰度显示图片

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -70,7 +70,7 @@ def plot_image(i, predictions_array, true_label, img):
     plt.xticks([])
     plt.yticks([])
 
-    plt.imshow(img, cmap=plt.cm.binary)
+    plt.imshow(img, cmap='binary')
 
     predicted_label = np.argmax(predictions_array)
     if predicted_label == true_label:
