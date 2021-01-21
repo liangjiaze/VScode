67588f307ad0986e51dedf1fdadfa50b88d4402b
liangjiaze <renmuda@163.com>
2021-01-21 15:31:24 +0800

    暂时屏蔽报错部分

--- RF_fashion.py
+++ RF_fashion.py
@@ -4,9 +4,7 @@ import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
-
 import pickle
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split,KFold
 from sklearn.metrics import *
 from sklearn.preprocessing import StandardScaler
@@ -87,14 +85,14 @@ RandomForest.predict_proba(test_images2)[0]
 print("随机森林准确率: {:.3f}".format(accuracy_score(test_labels,RandomForest.predict(test_images2))))
 
 # 可视化结果
-predictions_pro=RandomForest.predict_proba(test_images2)
-num_rows = 5
-num_cols = 3
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_images):
-  plt.subplot(num_rows, 2*num_cols, 2*i+1)
-  plot_image(i, predictions_pro, test_labels, test_images)
-  plt.subplot(num_rows, 2*num_cols, 2*i+2)
-  plot_value_array(i, predictions_pro, test_labels)
-plt.show()
+# predictions_pro=RandomForest.predict_proba(test_images2)
+# num_rows = 5
+# num_cols = 3
+# num_images = num_rows*num_cols
+# plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+# for i in range(num_images):
+#   plt.subplot(num_rows, 2*num_cols, 2*i+1)
+#   plot_image(i, predictions_pro, test_labels, test_images)
+#   plt.subplot(num_rows, 2*num_cols, 2*i+2)
+#   plot_value_array(i, predictions_pro, test_labels)
+# plt.show()
