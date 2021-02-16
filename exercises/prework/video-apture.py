9223c86f4e0b7fe3f03b056910230f687ce9780a
jade ankh <renmuda@163.com>
2021-02-16 12:59:39 +0800

    视频截取

--- exercises/prework/video-apture.py
+++ exercises/prework/video-apture.py
@@ -1,9 +1,8 @@
 # 从视频读取帧保存为图片
 from cv2 import cv2 as cv
 
-cap = cv.VideoCapture("D:/jade/develop/YOLO/yolov5/data/videos/GH010060.mp4")  #读取mp4文件
-
-# cap = cv.VideoCapture(0)  # 当使用的参数为一个数字0的时候代表从摄像头获取视频。
+# cap = cv.VideoCapture("D:/jade/develop/YOLO/yolov5/data/videos/GH010060.mp4")  #读取mp4文件，文件前一定要用“/”才可被识别不报错
+cap = cv.VideoCapture(0)  # 当使用的参数为一个数字0的时候代表从摄像头获取视频。
 
 c=0  #文件名从0开始
 
