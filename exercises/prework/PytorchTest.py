ca70202b7e27185562be3308098ed80021c98d1c
jade ankh <34931885+liangjiaze@users.noreply.github.com>
2021-08-27 18:27:15 +0800

    Update PytorchTest.py

--- exercises/prework/PytorchTest.py
+++ exercises/prework/PytorchTest.py
@@ -1,13 +1,22 @@
 import torch
-print(torch.__version__)
+
+print('Torch version: ' + torch.__version__)
 print('CUDA available: ' + str(torch.cuda.is_available()))
 print('cuDNN version: ' + str(torch.backends.cudnn.version()))
+print('device name: ' + torch.cuda.get_device_name(0))
+
+
 a = torch.cuda.FloatTensor(2).zero_()
 print('Tensor a = ' + str(a))
+
 b = torch.randn(2).cuda()
 print('Tensor b = ' + str(b))
+
 c = a + b
 print('Tensor c = ' + str(c))
+
+print(torch.rand(3,3).cuda())
+
 
 # import torchvision
 # print(torchvision.__version__)
